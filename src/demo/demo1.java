ee7a7d51e028f5449bcf5e7cd3474c9f842145be
vinaysreddy <d.vinaysreddy@gmail.com>
2025-11-04 14:00:47 -0600

    time pass

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -1,47 +1,12 @@
 package demo;
 
-import java.util.Arrays;
-
 public class demo1 {
-    public static void main(String[] args) {
-        int[] arr = {3, 1, 4, 2, 5};
-//        bubbleSort(arr);
-//        selectionSort(arr);
-//        System.out.println(Arrays.toString(arr));
+    final int x;
 
-//        bubbleRecursion(arr, 0, arr.length - 1);
-        int max = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
 
-            if (arr[i] > arr[i + 1]) {
-                max = arr[i];
-            }
-        }
-        System.out.printf(max + "  mnax");
+    String y = "latest masas";
 
-    }
-
-    private static void bubbleRecursion(int[] arr, int s, int e) {
-
-        if (e == 0) {
-            System.out.printf("bubble sort with recursion  " + Arrays.toString(arr));
-            return;
-        }
-
-        if (s < e) {
-            if (arr[s] > arr[s + 1]) {
-                swap(arr[s], arr[s + 1]);
-            }
-            bubbleRecursion(arr, s + 1, e);
-        } else {
-            bubbleRecursion(arr, s, e - 1);
-        }
-
-    }
-
-    private static void swap(int a, int b) {
-        int temp = a;
-        a = b;
-        b = temp;
+    demo1(int val) {
+        x = val;
     }
 }
